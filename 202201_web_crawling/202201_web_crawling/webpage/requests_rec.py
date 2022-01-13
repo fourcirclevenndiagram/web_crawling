be66e2d8454f935368499ee5c2450a0ff8ab9512
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-13 20:58:42 +0900

    googleをｗ

--- 202201_web_crawling/202201_web_crawling/webpage/requests_rec.py
+++ 202201_web_crawling/202201_web_crawling/webpage/requests_rec.py
@@ -4,7 +4,8 @@ res = requests.get("http://dnet.nkcnet.co.jp/scripts/dneo/dneo.exe?")
 res2 = requests.get("http://google.com")
 print("応答コード : ", res.status_code) # 200なら正常
 print("応答コード : ", res2.status_code) # 200なら正常
-
+res2.raise_for_status()
+print("ウェブスクレイピングを開始します")
 # if res.status_code == requests.codes.ok:
 #     print("正常です")
 # else:
@@ -17,5 +18,8 @@ print("応答コード : ", res2.status_code) # 200なら正常
 
 # res.raise_for_status()                   # Exception has occurred: HTTPError
 # print("ウェブスクレイピングを開始します")   # Exception has occurred: HTTPError
-res2.raise_for_status()
-print("ウェブスクレイピングを開始します")
+
+print(len(res.text))
+print(res2.text)
+with open("mygoogle.html", "w", encoding="utf8") as f:
+    f.write(res2.text)
