ea5b3c3b2a83248a9c0832c11a0a6630f2acea4d
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-13 21:15:34 +0900

    正規表現テスト

---
+++ 202201_web_crawling/202201_web_crawling/webpage/_re.py
@@ -0,0 +1,14 @@
+import re
+# abcd, book, desk
+# ca?e -> care, cafe, case, cave
+# caae, cabe, cace, ...
+
+p = re.compile("ca.e")
+# . (ca.e) : 一つの文字     ex) care, cafe, case // caffe (X)
+# ^ (^de)  : 文字列の始まり ex) desk, destination  // fade (X)
+# $ (se$)  : 文字列の最後   ex) case, base // face (X)
+
+m = p.match("caffe")
+n = p.match("case")
+print(m.group())    # マッチングに失敗したらエラー発生
+print(n.group())    # マッチングに失敗したらエラー発生
