1651e8225875b01cbc12ce9596ccb01e6e5b55de
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-13 18:14:49 +0900

    ウェブサイト変更記載

--- 202201_web_crawling/202201_web_crawling/webpage/requests_rec.py
+++ 202201_web_crawling/202201_web_crawling/webpage/requests_rec.py
@@ -1,4 +1,6 @@
 import requests
 # res = requests.get("http://naver.com")
 res = requests.get("https://pinocc.tistory.com/198")
+res2 = requests.get("D:\05_Programming\Python\202201_web_crawling\202201_web_crawling\202201_web_crawling\webpage\main.html")
 print("応答コード : ", res.status_code) # 200なら正常
+print("応答コード : ", res2.status_code) # 200なら正常
