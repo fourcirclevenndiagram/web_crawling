610e4597f18726ca5ec5f20744fed1ab245eb684
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-16 19:48:45 +0900

    soup関数挿入

--- 202201_web_crawling/202201_web_crawling/_bs4.py
+++ 202201_web_crawling/202201_web_crawling/_bs4.py
@@ -1,2 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = "https://comic.naver.com/webtoon/weekday"
+res = requests.get(url)
+res.raise_for_status()
+
+soup = BeautifulSoup(res.text, "lxml")
+print(soup.title)
