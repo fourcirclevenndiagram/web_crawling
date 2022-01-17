6835c6ef4b155d205ce80c8412274cd242b92169
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-17 20:47:07 +0900

    siblingを多数召喚してみた

--- 202201_web_crawling/202201_web_crawling/_bs4.py
+++ 202201_web_crawling/202201_web_crawling/_bs4.py
@@ -8,9 +8,22 @@ res.raise_for_status()
 soup = BeautifulSoup(res.text, "lxml")
 print(soup.title)
 print(soup.title.get_text())
-print(soup.a)
-print(soup.a.attrs)
+print("soup.a       : ", soup.a)
+print("soup.a.attrs : ", soup.a.attrs)
 print(soup.a["href"])
 
 print(soup.find("a", attrs={"class":"Nbtn_upload"}))
 print(soup.find(attrs={"class":"Nbtn_upload"}))
+
+print("liタグの情報採取")
+print(soup.find("li", attrs={"class":"rank01"}))
+rank01 = soup.find("li", attrs={"class":"rank01"})
+print(rank01.a)
+print(rank01.a.get_text())
+# print(rank01.next_sibling)   # このsiblingに該当する情報は単なる改行情報なのか、なにも表示されない
+print(rank01.next_sibling.next_sibling)
+rank02 = rank01.next_sibling.next_sibling
+rank03 = rank02.next_sibling.next_sibling
+print(rank03.a.get_text())
+
+print(rank01.parent)
