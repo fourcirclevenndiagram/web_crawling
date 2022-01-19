477b01d7dc06b411441bbf0ef3416db525744d32
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-01-19 21:15:13 +0900

    一つの作品のページの、1ページ目のエピソードとそのURLをすべて引き抜く

--- 202201_web_crawling/202201_web_crawling/_bs4.py
+++ 202201_web_crawling/202201_web_crawling/_bs4.py
@@ -47,4 +47,11 @@ soup = BeautifulSoup(res.text, "lxml")
 
 cartoons = soup.find_all("td", attrs={"class":"title"})
 title = cartoons[0].a.get_text()
+link = cartoons[0].a["href"]
 print(title)
+print("https://comic.naver.com" + link)
+
+for cartoon in cartoons:
+    title = cartoon.a.get_text()
+    link = "https://comic.naver.com" + cartoon.a["href"]
+    print(title, link)
